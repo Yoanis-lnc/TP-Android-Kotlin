8d3fecfd6ce23b0553687b3e8411f37fdf10b23c
Yoanis <yoanislance39@gmail.com>
2023-01-14 18:46:42 +0100

    Créeation projet plus page principale

--- app/src/main/java/com/example/swapi/MainActivity.kt
+++ app/src/main/java/com/example/swapi/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.swapi
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +17,15 @@ class MainActivity : AppCompatActivity() {
 
         val backgroundImage = resources.getIdentifier("fondsw", "drawable", packageName)
         findViewById<View>(android.R.id.content).setBackgroundResource(backgroundImage)
+
+        val mediaPlayer = MediaPlayer.create(this, R.raw.starwars)
+        mediaPlayer.start()
+
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://swapi.dev/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
 
     }
